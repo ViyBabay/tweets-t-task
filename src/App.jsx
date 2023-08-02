9c41fc25d8295b7de88c0f624218ae9db08fd6ac
Александр Пикуш <pikush.oleksandr@gmail.com>
2023-08-02 22:03:03 +0300

    UPD:  App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Tweets from "./pages/Tweets/Tweets";
 import { Layout } from "./components/Layout/Layout";
@@ -6,13 +6,13 @@ import { Home } from "./pages/Home/Home";
 
 export const App = () => {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="/users" element={<Tweets />} />
         </Route>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 };
