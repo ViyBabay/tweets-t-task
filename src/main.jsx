8aa1c0ab475227b32a3b8ef88d28127378729fd3
Александр Пикуш <pikush.oleksandr@gmail.com>
2023-08-02 21:17:26 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename="/tweets_t-task">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>
